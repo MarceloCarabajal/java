b6a4d4b5a3d6100ee74cfa097bec0b7966c642e2
MarceloCarabajal <marce.tsgia@gmail.com>
2024-08-31 12:45:17 -0300

    Numeros Primitivos

---
+++ IdeaProjects/HolaMundo/src/main/java/org/example/PrimitivosFloat.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+        
+        float realFloat = 2.12e3f; //es equivalente a 2120f;
+        float realFloat2 = 1.5e4f; //es equivalente a 15000f;
+        float realFloat3 = 1.5e-10f; //es equivalente a 000000000015f;
+
+        System.out.println("realFloat = " + realFloat);
+        System.out.println("realFloat2 = " + realFloat2);
+        System.out.println("realFloat3 = " + realFloat3);
+    }
+}
